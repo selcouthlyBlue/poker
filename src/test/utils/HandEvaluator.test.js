1d158dd8fa7efab929624126974b5959dbebc97e
Jerome Gonzalvo <jgonzalvo@dyl.com>
2018-10-19 13:18:56 +0800

    Refactored HandEvaluatorTest to the tests read better.

--- src/test/utils/HandEvaluator.test.js
+++ src/test/utils/HandEvaluator.test.js
@@ -5,39 +5,42 @@ import FACES from "entity/FACES";
 import HandEvaluator from "utils/HandEvaluator";
 
 describe('Evaluate poker hands', () => {
+    let hand = [];
+
+    function handShouldEvaluateTo(expected) {
+        expect(HandEvaluator.evaluate(hand)).toEqual(expected);
+    }
+
     it('should give HIGH CARD when hand does not form a combination', () => {
-        let hand = Hand([
+        hand = Hand([
             Card(SUITS.DIAMONDS, FACES.THREE),
             Card(SUITS.CLUBS, FACES.JACK),
             Card(SUITS.SPADES, FACES.EIGHT),
             Card(SUITS.HEARTS, FACES.FOUR),
             Card(SUITS.SPADES, FACES.TWO)
         ]);
-        let result = HandEvaluator.evaluate(hand);
-        expect(result).toEqual("HIGH CARD");
+        handShouldEvaluateTo("HIGH CARD");
     });
 
     it('should give PAIR when hand has a pair', () => {
-        let hand = Hand([
+        hand = Hand([
             Card(SUITS.HEARTS, FACES.ACE),
             Card(SUITS.DIAMONDS, FACES.ACE),
             Card(SUITS.CLUBS, FACES.EIGHT),
             Card(SUITS.SPADES, FACES.FOUR),
             Card(SUITS.HEARTS, FACES.SEVEN)
         ]);
-        let result = HandEvaluator.evaluate(hand);
-        expect(result).toEqual("PAIR");
+        handShouldEvaluateTo("PAIR");
     });
 
     it('should give TWO PAIR when hand has two different pairs', () => {
-        let hand = Hand([
+        hand = Hand([
             Card(SUITS.CLUBS, FACES.FOUR),
             Card(SUITS.SPADES, FACES.FOUR),
             Card(SUITS.CLUBS, FACES.THREE),
             Card(SUITS.DIAMONDS, FACES.THREE),
             Card(SUITS.CLUBS, FACES.QUEEN)
         ]);
-        let result = HandEvaluator.evaluate(hand);
-        expect(result).toEqual("TWO PAIR");
+        handShouldEvaluateTo("TWO PAIR");
     });
 });
